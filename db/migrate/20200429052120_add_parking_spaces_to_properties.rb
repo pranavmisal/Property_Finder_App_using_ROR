a0c16d62c64882c39b4622c241e4f465311a7ef6
Pranav Misal <pranav.misal@definelabs.in>
2020-04-29 11:24:48 +0530

    to show price commit

--- db/migrate/20200429052120_add_parking_spaces_to_properties.rb
+++ db/migrate/20200429052120_add_parking_spaces_to_properties.rb
@@ -1,5 +1,5 @@
 class AddParkingSpacesToProperties < ActiveRecord::Migration[6.0]
-  def change
-    add_column :properties, :parking_spaces, :integer
-  end
+	def change
+		add_column :properties, :parking_spaces, :integer, default: 0
+	end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_28_042513) do
+ActiveRecord::Schema.define(version: 2020_04_29_052120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2020_04_28_042513) do
     t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "parking_spaces", default: 0
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
